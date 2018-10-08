4ab626bc7d79a0b5b29db6581ebcc6d6794b6654
Riya Patel <riaupatel13@yahoo.com>
2018-10-08 11:00:10 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a10016322/midtermreview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.a10016322.midtermreview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+//to change app icon, change android:icon="@drawable/image name" in Android Manifest and put image in drawable folder
+//intents - intentdemo app
+//editText - name.addTextChanged... - widgetsOne
+//switch - widgetsOne
+//radioGroup - widgetTwo
+//Toast - widgetTwo
+//spinner - lists
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
